013ddd863e20b7adbacb0507ec2b1c6033cb6908
Nageshwar Reddy Pandem <nageshwar521@Nageshwars-MacBook-Air.local>
2021-03-22 23:16:56 +0530

    jwt exp time syntax updated

--- src/auth/auth.utils.js
+++ src/auth/auth.utils.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { getSecretAccessToken } = require('../common/common.utils');
 
 const createAuthToken = (user, secretToken) => {
-  return jwt.sign(user, secretToken, { expiresIn: '5m' });
+  return jwt.sign(user, secretToken, { expiresIn: 5 * 60 });
 };
 
 module.exports = { createAuthToken };
